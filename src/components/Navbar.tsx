07aa8bb3743070012f8c1249a6ee5610a9d70164
AnkitNayan83 <ankitnayan83@gmail.com>
2023-11-16 15:23:51 +0530

    commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,16 +9,25 @@ const Navbar = () => {
     const { user, logout } = useAuth();
     const handelLogout = () => {
         router.push("/login");
+        localStorage.clear();
         logout();
     };
     return (
         <div className="flex items-center justify-between p-4 bg-[#666]">
             <h1 className="text-2xl font-bold text-white">Admin Panel</h1>
             <div className="flex items-center gap-6 text-white">
-                <span className="text-xl capitalize">{user?.username}</span>
-                <span className="text-xl cursor-pointer" onClick={handelLogout}>
-                    logout
-                </span>
+                {!user ? (
+                    <span className="text-xl cursor-pointer" onClick={handelLogout}>
+                        login
+                    </span>
+                ) : (
+                    <>
+                        <span className="text-xl capitalize">{user?.username}</span>
+                        <span className="text-xl cursor-pointer" onClick={handelLogout}>
+                            logout
+                        </span>
+                    </>
+                )}
             </div>
         </div>
     );
